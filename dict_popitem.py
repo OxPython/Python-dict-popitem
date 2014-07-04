b0c8c5e48a5b74f921a9c2a3665a319ce051c2b5
Emerson <root@emerson-N53SN>
2014-07-04 05:46:22 -0500

    Howto remove an arbitrary element from a dict in python?

--- dict_popitem.py
+++ dict_popitem.py
@@ -1,26 +1,23 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+Howto remove an arbitrary element from a dict in python?
+
+¿Cómo eliminar un elemento arbitrario de un dict en python?
+
 '''
-############################################################
-#Howto remove an arbitrary element from a dict in python?
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
      "blue" : 300
      }
-
 print(d)
 
 # Removing an "arbitrary" element means that the function can remove whatever 
 # item it likes. That doesn't mean that it has to be especially random about it.
-
 d.popitem()
-
-print(d)
-
 print(d)
